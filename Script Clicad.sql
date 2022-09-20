7dbce2670663d67f7445d8752928cd35d47d22e6
Maiara Santos <maiarasantos34@yahoo.com>
2022-09-19 22:35:44 -0300

    Add files via upload

---
+++ Script Clicad.sql
@@ -0,0 +1,36 @@
+/*
+-- Query: SELECT * FROM coursejdbc.clicad
+LIMIT 0, 1000
+
+-- Date: 2022-09-19 22:33
+*/
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (1,'Francisco ','Rua da Paz, 175, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (2,'Andressa ','Rua Antônio Carlos, 185, Bairro da Cascalheira, Tucano,Bahia','48790-000','2021-06-18','Feminino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (3,'Fabricio','Rua da Paz, 175, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (4,'Alexandre','Avenida Presidente Kennedy, 785, centro, Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (5,'Adriano','Rua da Paz, 175, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (6,'Mauricio','Praça Vigário Martins, 175, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (7,'Marcela','Rua da Paz, 800, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Feminino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (8,'Cristina','Rua da Paz, 400, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Feminino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (9,'Sarah','Rua da Paz, 175, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Feminino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (10,'Humberto','Rua da Cachoeira, 145, Bairro da Cascalheira, Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (11,'Fernanda','Rua Antônio brito, 100, Bairro da Bombinha, Tucano,Bahia','48790-000','2021-06-18','Feminino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (12,'Emanuel','Rua da Paz, 176, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (13,'Jeferson','Avenida ACM, 200, Centro, Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (14,'José','Avenida ACM, 145, Centro, Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (15,'José','Rua Carlos Sousa Silva, 574, Bairro da Laranjeira Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (16,'José','Rua Carlos Sousa Silva, 6784, Bairro da Laranjeira, Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (17,'José','Rua Carlos Sousa Silva, 456, Bairro da Laranjeira, Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (18,'José','Rua da Flores, 777, Bairro da Laranjeira Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (19,'Fabiana','Rua Antônio Sousa , 445, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Feminino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (20,'Jenifer','Rua Conslheiro, 145, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Feminino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (21,'João','Rua Conslheiro, 175, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Feminino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (24,'João','Rua Conslheiro, 175, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Feminino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (24,'João','Rua Conslheiro, 175, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Feminino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (25,'Guilerme','Rua Conslheiro, 175, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (25,'Guilerme','Rua Conslheiro, 175, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (25,'Guilerme','Rua Conslheiro, 175, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (25,'Guilerme','Rua Conslheiro, 175, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (25,'Guilerme','Rua Conslheiro, 175, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (25,'Guilerme','Rua Conslheiro, 175, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Masculino');
+INSERT INTO `` (`Clicod`,`Clinome`,`Cliend`,`Clicep`,`Clidata`,`Cligenero`) VALUES (26,'Jão','Rua Conslheiro, 175, Bairro da Creche, Tucano,Bahia','48790-000','2021-06-18','Masculino');
